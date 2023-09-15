ba9932b60263a722d6cfe9472a01a4aab7126e26
clarence <tegumclarence@gmail.com>
2023-09-15 07:20:56 +0100

    Create solver class , add method fraction reverse

---
+++ solver.rb
@@ -0,0 +1,17 @@
+class Solver
+  def factorial(number)
+    if number.negative?
+      raise ArgumentError, 'Input must be a non-negative integer'
+    elsif number.zero?
+      1
+    else
+      result = 1
+      (1..number).each { |i| result *= i }
+      result
+    end
+  end
+
+  def reverse(word)
+    word.reverse
+  end
+end
